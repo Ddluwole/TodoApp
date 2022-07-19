2be69d9dd8b9fc2bedb3a87ea9ae6e69dbf940bc
David Ilori <ddluwole@gmail.com>
2022-07-19 16:42:16 +0100

    Setting up

--- Todo_project/settings.py
+++ Todo_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +25,10 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2k10!e80q5vb84%i#&!5csuntfk44fyf@1*%wza*bn*2iy-7h6'
+SECRET_KEY =os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -43,7 +45,7 @@ INSTALLED_APPS = [
 
     #LOCAL APPS
     'accounts',
-    'todo_app',
+    'todo',
 ]
 
 MIDDLEWARE = [
